93798a3a9741ec9e25956349f18c7e52ccc8165f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-02-04 18:09:17 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000 ?? process.env.PORT
+const DIR = process.cwd()
+
+app.post('/create', (req, res) => {
+  let body = ''
+  req.on('data', (chunk) => body += chunk)
+  req.on('end', () => body.toString())
+
+  console.log(body)
+  res.send('listo, body:', body)
+})
+
+app.use('/', express.static(DIR))
+app.listen(PORT, () => `server on port:${PORT}`)
